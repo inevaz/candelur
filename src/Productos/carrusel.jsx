8c504b643b7573572d87204fd547c43f82a64805
Emiliano-Etcheverry <131318332+Emiliano-Etcheverry@users.noreply.github.com>
2025-05-21 01:05:39 +0000

    Primeras entradas

---
+++ src/Productos/carrusel.jsx
@@ -0,0 +1,11 @@
+
+
+const Carrusel = () => {
+    return (
+        <div className='h-full w-full max-w-screen-lg'>
+            
+        </div>
+    )
+}
+
+export default Carrusel
--- src/Productos/productos.jsx
+++ src/Productos/productos.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Carrusel from '../Productos/carrusel'
 
 const Productos = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6 dark:text-white">Productos</h1>
+      <Carrusel/>
     </div>
   );
 };
